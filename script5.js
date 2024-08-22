21a9cb65c7b76acf40daf5bb06d79ce773722c24
sahilj18 <sahilmernj18@proton.me>
2024-08-22 08:02:16 +0000

    save it

--- script5.js
+++ script5.js
@@ -1,27 +1,40 @@
-"use strict"
+"use strict";
 
 function move(char, step) {
-const alpha = "abcdefghijklmnopqrstuvwxyz";
-const alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let alpha = "abcdefghijklmnopqrstuvwxyz";
 
-const indexChar = alpha.indexOf(char);
-let finalIndex = indexChar + step;
+  if (!alpha.includes(char)) {
+    if (alpha.toUpperCase().includes(char)) {
+      alpha = alpha.toUpperCase();
+    } else {
+      return char;
+    }
+  }
+  const indexChar = alpha.indexOf(char);
+  let finalIndex = indexChar + step;
 
-if (finalIndex > 25) {
+  if (finalIndex > 25) {
     finalIndex = finalIndex % 26;
-}
+  }
 
-const finalChar = alpha[finalIndex];
-return finalChar;
+  const finalChar = alpha[finalIndex];
+  return finalChar;
 
-//Find index of char
-// let index = alpha.indexOf(char);
-// index += 1;
-// console.log(alpha[index]);
+  //Find index of char
+  // let index = alpha.indexOf(char);
+  // index += 1;
+  // console.log(alpha[index]);
 }
-const x = move ("z", 4);
-console.log(x);
-const y = move ("D", 6);
-console.log(y);
+// const x = move("middle-Outz", 2);
+// console.log(x);
 
 // move1("f");
+function caesarCipher(sentence, count){
+  let finalstr = "";
+  for (let i=0; i<sentence.length;i++){
+    finalstr+= move(sentence[i],count)
+  }
+    return finalstr;
+}
+
+console.log(caesarCipher("middle-out",2));
